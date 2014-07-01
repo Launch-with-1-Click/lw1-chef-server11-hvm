fc449c47bd64579f3e6d9afed8cbd72d3538400a
sawanoboly <sawanoboriyu@higanworks.com>
2014-07-01 22:45:28 +0900

    v11.1.3

--- cleanup.sh
+++ cleanup.sh
@@ -1,32 +1,28 @@
-#!/usr/env/bin bash
+#!/usr/bin/env bash
 
 set -ex
 
-echo "Yes? or Ctrl+C"
-read hoge
+sudo apt-get clean all
 
-apt-get clean all
-
-chef-server-ctl stop
-chef-server-ctl cleanse
-rm -rf /root/chef-server-cleanse-*
+sudo chef-server-ctl stop
+sudo chef-server-ctl cleanse
+sudo rm -rf /root/chef-server-cleanse-*
 sudo install -d /etc/chef-server
-sudo install -o root -g root -m 0640 /vagrant/files/chef-server.rb /etc/chef-server/
+sudo install -o root -g root -m 0640 /tmp/chef-server/files/chef-server.rb /etc/chef-server/
 
 
-rm -rf /tmp/*
-rm -rf /vagrant
-rm -f /etc/ssh/ssh_host_*
+sudo rm -rf /tmp/*
+sudo rm -f /etc/ssh/ssh_host_*
 cd /var/log
-find /var/log/ -type f -name '*.log' -exec cp /dev/null {} \;
-cp /dev/null /var/log/syslog
+find /var/log/ -type f -name '*.log' -exec sudo cp /dev/null {} \;
+sudo cp /dev/null /var/log/syslog
 
 
-yes | cp /dev/null /root/.ssh/authorized_keys
-yes | cp /dev/null /root/.bash_history
+yes | sudo cp /dev/null /root/.ssh/authorized_keys
+yes | sudo cp /dev/null /root/.bash_history
 if [ -d /home/ubuntu ]; then
-  yes | cp /dev/null /home/ubuntu/.ssh/authorized_keys
-  yes | cp /dev/null /home/ubuntu/.bash_history
+  yes | sudo cp /dev/null /home/ubuntu/.ssh/authorized_keys
+  yes | sudo cp /dev/null /home/ubuntu/.bash_history
 fi
 history -c
 
